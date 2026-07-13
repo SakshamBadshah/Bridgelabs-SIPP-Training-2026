e6e2fa1e51c9590ecae16ede622d1dfceb22c382
Saksham Chaturvedi <your-email@gmail.com>
2026-07-13 14:42:57 +0530

    Update code

---
+++ core-java-practice/gcr-codebase/SortingAlgorithms_Bubble_Selection_Insertion Sort/Insertion_sort/InsertionSortDemo.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class InsertionSortDemo {
+
+    public static void main(String[] args) {
+
+        int nums[] = {12, 11, 13, 5, 6};
+
+        for (int i = 1; i < nums.length; i++) {
+
+            int key = nums[i];
+            int j = i - 1;
+
+            while (j >= 0 && nums[j] > key) {
+
+                nums[j + 1] = nums[j];
+                j--;
+            }
+
+            nums[j + 1] = key;
+        }
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
